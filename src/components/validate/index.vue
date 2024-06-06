a261b39c1d2f51dab5f23fb75b46cfb0268b1d35
Mary Kilduff <marykilduff@Marys-MacBook-Pro-3.local>
2024-06-06 16:22:42 +0100

    using library and validation schema

--- src/components/validate/index.vue
+++ src/components/validate/index.vue
@@ -1,13 +1,8 @@
 <template>
-  <Form>
+  <Form @submit="onSubmit" :validation-schema="formSchema">
     <div class="mb-3">
       <label for="name">Name</label>
-      <Field
-        name="name"
-        :rules="[isRequired, validateName]"
-        placeholder="Enter your name"
-        class="form-control"
-      />
+      <Field name="name" placeholder="Enter your name" class="form-control" />
       <ErrorMessage name="name" as="div" v-slot="{ message }">
         <div class="alert alert-danger" role="alert">
           {{ message }}
@@ -21,18 +16,14 @@
 
 <script setup>
 import { Field, Form, ErrorMessage } from "vee-validate";
+import * as yup from "yup";
 
-const isRequired = (value) => {
-  if (!value) {
-    return "This field is required";
-  }
-  return true;
-};
+const formSchema = yup.object({
+  name:yup.string().required('The name is required').max(20, 'Sorry a max of 20 characters')
+})
 
-const validateName = (value) => {
-  if (value !== "steve") {
-    return true;
-  }
-  return "You are not allowed steve !!!";
-};
+function onSubmit(values, { resetForm }) {
+  console.log(values);
+  resetForm();
+}
 </script>
